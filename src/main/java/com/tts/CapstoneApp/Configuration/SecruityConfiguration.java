3dc245b65b5d2b4b17c59819b63048a81e1afe39
Lionel Beato <lionel.beato@gmail.com>
2021-07-29 11:50:19 -0400

    undo mistake with sub

--- src/main/java/com/tts/CapstoneApp/Configuration/SecruityConfiguration.java
+++ src/main/java/com/tts/CapstoneApp/Configuration/SecruityConfiguration.java
@@ -36,8 +36,8 @@ public class SecruityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
 
-                .cors()
-                .and()
+//                .cors()
+//                .and()
 //                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
